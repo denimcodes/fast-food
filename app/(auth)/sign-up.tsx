20fdc93786ae24518aadfff1d25fd4edbc6c4b17
Denimsun Basumatary <denimbasumatary@outlook.com>
2026-01-30 06:44:26 +0530

    Add auth screens

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,41 @@
-import { router } from 'expo-router'
-import { View, Text, Button } from 'react-native'
+import CustomButton from '@/components/CustomButton'
+import CustomInput from '@/components/CustomInput'
+import { Link, router } from 'expo-router'
+import { useState } from 'react'
+import { View, Text, Button, Alert } from 'react-native'
 
 const SignUp = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setForm] = useState({ name: '', email: '', password: '' });
+
+  const submit = async () => {
+    if (!form.name || !form.email || !form.password) return Alert.alert('Error', 'Please enter valid name, email address and password')
+
+    setIsSubmitting(true)
+
+    try {
+      // Call appwrite sign up function
+      
+      Alert.alert('Success', 'User signed up successfully')
+      router.replace('/')
+    } catch (error: any) {
+      Alert.alert('Error', error.message)
+    } finally {
+      setIsSubmitting(false)
+    }
+  }
+
   return (
-    <View>
-      <Text>SignUp</Text>
-      <Button title='Sign in' onPress={() => router.push('/sign-in')} />
+    <View className='gap-10 bg-white rounded-lg p-5 mt-5'>
+      <CustomInput placeholder='Enter name' value={form.name} onChangeText={(text) => {setForm((prev) => ({...prev, name: text}))}} label='Full name' />
+      <CustomInput placeholder='Enter email address' value={form.email} onChangeText={(text) => {setForm((prev) => ({...prev, email: text}))}} label='Email' keyboardType='email-address' />
+      <CustomInput placeholder='Enter password' value={form.password} onChangeText={(text) => { setForm((prev) => ({...prev, password: text}))}} label='Password' secureTextEntry={true} />
+      <CustomButton title='Sign up' isLoading={isSubmitting} onPress={submit} />
+
+      <View className='flex justify-center mt-5 flex-row gap-2'>
+        <Text className='base-regular text-gray-100'>Already have an account?</Text>
+        <Link href='/sign-in' className='base-bold text-primary'>Sign in</Link>
+      </View>
     </View>
   )
 }
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,7 @@
 export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   platform: "in.denim.fooddelivery",
-  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: '697b5f1e00347ddce219',
+  userTableid: 'user'
 }
