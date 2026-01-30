3acd26d61701895c3ac963695b45eb5d08d9e89c
Denimsun Basumatary <denimbasumatary@outlook.com>
2026-01-30 08:50:28 +0530

    Setup auth functionality

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,5 +1,6 @@
 import CustomButton from '@/components/CustomButton'
 import CustomInput from '@/components/CustomInput'
+import { signIn } from '@/lib/appwrite'
 import { Link, router } from 'expo-router'
 import { useState } from 'react'
 import { View, Text, Button, Alert } from 'react-native'
@@ -9,17 +10,18 @@ const SignIn = () => {
   const [form, setForm] = useState({ email: '', password: '' });
 
   const submit = async () => {
-    if (!form.email || !form.password) return Alert.alert('Error', 'Please enter valid email address and password')
+    const {email, password} = form;
+    if (!email || !password) return Alert.alert('Error', 'Please enter valid email address and password')
 
     setIsSubmitting(true)
 
     try {
-      // Call appwrite sign in function
+      await signIn({email, password});
       
       Alert.alert('Success', 'User signed in successfully')
       router.replace('/')
-    } catch (error: any) {
-      Alert.alert('Error', error.message)
+    } catch (e: any) {
+      Alert.alert('Error', e.message)
     } finally {
       setIsSubmitting(false)
     }
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,7 +1,59 @@
+import { CreateUserParams, SignInParams } from "@/type";
+import { Account, Avatars, Client, Databases, ID, Query, TablesDB } from "react-native-appwrite"
+
 export const appwriteConfig = {
-  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
   platform: "in.denim.fooddelivery",
-  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
   databaseId: '697b5f1e00347ddce219',
   userTableid: 'user'
+}
+
+export const client = new Client();
+
+client.setEndpoint(appwriteConfig.endpoint).setPlatform(appwriteConfig.platform).setProject(appwriteConfig.projectId)
+
+export const account = new Account(client)
+export const tablesDb = new TablesDB(client)
+export const avatars = new Avatars(client)
+
+export const createUser = async ({ email, password, name }: CreateUserParams) => {
+  try {
+    const newAccount = await account.create({ userId: ID.unique(), email, password, name });
+    if (!newAccount) throw Error('Failed to create account');
+
+    await signIn({ email, password })
+
+    const avatarUrl = avatars.getInitialsURL(name)
+    return tablesDb.createRow({ databaseId: appwriteConfig.databaseId, tableId: appwriteConfig.userTableid, rowId: ID.unique(), data: { name, email, accountId: newAccount.$id, avatar: avatarUrl } })
+  } catch (e) {
+    throw new Error(e as string)
+  }
+}
+
+export const signIn = async ({ email, password }: SignInParams) => {
+  try {
+    await account.createEmailPasswordSession({ email, password })
+  } catch (e) {
+    throw new Error(e as string)
+  }
+}
+
+export const getCurrentUser = async () => {
+  try {
+    const currentAccount = await account.get();
+    if (!currentAccount) throw Error('No current account')
+
+    const currentUser = await tablesDb.listRows({
+      databaseId: appwriteConfig.databaseId,
+      tableId: appwriteConfig.userTableid,
+      queries: [Query.equal('accountId', currentAccount.$id)]
+    })
+
+    if (!currentUser) throw Error('No current user')
+    return currentUser.rows[0];
+  } catch (e) {
+    console.error(e)
+    throw new Error(e as string)
+  }
 }
